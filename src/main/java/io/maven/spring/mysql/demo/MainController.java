5515f1fcbd02798b922639fe88e1c433389db222
Selvapraveen <selvapraveen0405@gmail.com>
2024-09-07 07:55:10 +0530

    Commit

---
+++ src/main/java/io/maven/spring/mysql/demo/MainController.java
@@ -0,0 +1,22 @@
+package io.maven.spring.mysql.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/demo")
+public class MainController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path = "/add")
+    public @ResponseBody User addNewUser(@RequestParam String name,
+                                         @RequestParam String email) {
+        User springUser = new User();
+        springUser.setName(name);
+        springUser.setEmail(email);
+        userRepository.save(springUser);
+        return springUser;
+    }
+}
