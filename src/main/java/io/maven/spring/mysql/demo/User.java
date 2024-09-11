598ec6797c4f59c8c603491f4f2a2c5e0eec27fd
Selvapraveen <selvapraveen0405@gmail.com>
2024-09-11 05:43:34 +0530

    Commit

--- src/main/java/io/maven/spring/mysql/demo/User.java
+++ src/main/java/io/maven/spring/mysql/demo/User.java
@@ -12,4 +12,22 @@ public class User {
     private Integer id;
     private String name;
     private String email;
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEmail() {
+        return email;
+    }
 }
